1230c19a418e09f6f7c7eb331c7af298692dd9ba
Vlad <87645405+Fr1senn@users.noreply.github.com>
2022-06-14 13:47:55 +0300

    Implemented views and urls for reviews and catalog

--- bookStore/store/urls.py
+++ bookStore/store/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('reviews/', views.ReviewsView.as_view(), name='reviews'),
+
+    path('catalog/', views.CatalogView.as_view(), name='catalog'),
 ]
--- bookStore/store/views.py
+++ bookStore/store/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
 from django.views.generic import ListView, TemplateView, DetailView, CreateView
 
+from . import models
+
 
 class HomeView(TemplateView):
     template_name = 'store/index.html'
+
+
+class ReviewsView(ListView):
+    template_name = 'store/reviews.html'
+    context_object_name = 'reviews'
+
+    def get_queryset(self):
+        return models.Review.objects.exclude(user__username='admin')
+
+
+class CatalogView(ListView):
+    template_name = 'store/catalog.html'
+    context_object_name = 'books'
+
+    def get_queryset(self):
+        return models.Book.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['authors'] = models.Author.objects.all()
+        context['genres'] = models.Genre.objects.all()
+        return context
