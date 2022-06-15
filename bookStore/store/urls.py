96636c65265ebdb89efda7f5ca97f7e422513776
Vlad <87645405+Fr1senn@users.noreply.github.com>
2022-06-15 12:34:20 +0300

    Implemented display book by author and genre and single book

--- bookStore/store/urls.py
+++ bookStore/store/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('reviews/', views.ReviewsView.as_view(), name='reviews'),
+    path('book/<slug:slug>/', views.BookDetail.as_view(), name='book'),
 
     path('catalog/', views.CatalogView.as_view(), name='catalog'),
+    path('catalog/<slug:slug>/', views.BookByGenreView.as_view(), name='book_by_genre'),
+    path('catalog/<slug:slug>/', views.BookByAuthorView.as_view(), name='book_by_author'),
 ]
