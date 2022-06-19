b128e4677493c85459f37dadba66bdaa138a94d4
Vlad <87645405+Fr1senn@users.noreply.github.com>
2022-06-19 12:52:16 +0300

    Added url for the profile

--- bookStore/store/urls.py
+++ bookStore/store/urls.py
@@ -6,10 +6,11 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('reviews/', views.ReviewsView.as_view(), name='reviews'),
     path('book/<slug:slug>/', views.BookDetail.as_view(), name='book'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
 
     path('catalog/', views.CatalogView.as_view(), name='catalog'),
     path('catalog/genre/<slug:slug>/', views.BookByGenreView.as_view(), name='book_by_genre'),
     path('catalog/author/<slug:slug>/', views.BookByAuthorView.as_view(), name='book_by_author'),
 
-    path('accounts/registration/', views.RegistrationView.as_view(), name='registration'),
+    path('registration/', views.RegistrationView.as_view(), name='registration'),
 ]
