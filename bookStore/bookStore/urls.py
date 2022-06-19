37dec4469ec43d0dedb86e4229d3513156e0858b
Vlad <87645405+Fr1senn@users.noreply.github.com>
2022-06-19 12:52:31 +0300

    Minor changes

--- bookStore/bookStore/urls.py
+++ bookStore/bookStore/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('debug/', include('debug_toolbar.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('django.contrib.auth.urls')),
     path('', include('store.urls')),
 ]
 
