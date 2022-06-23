f6aa3466e2794fa99f429e6e1e63f2d230fea9e2
Vlad <87645405+Fr1senn@users.noreply.github.com>
2022-06-23 12:21:38 +0300

    Minor changes

--- bookStore/store/urls.py
+++ bookStore/store/urls.py
@@ -3,15 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
-
     path('reviews/', views.ReviewsView.as_view(), name='reviews'),
     path('reviews/create/', views.CreateReviewView.as_view(), name='create_review'),
 
     path('book/<slug:slug>/', views.BookDetail.as_view(), name='book'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
 
-    path('catalog/', views.CatalogView.as_view(), name='catalog'),
+    path('', views.CatalogView.as_view(), name='catalog'),
     path('catalog/genre/<slug:slug>/', views.BookByGenreView.as_view(), name='book_by_genre'),
     path('catalog/author/<slug:slug>/', views.BookByAuthorView.as_view(), name='book_by_author'),
 
