7e2f5b0d6e1e1063f41d27a5058dd4e5c0cda03d
Vlad <87645405+Fr1senn@users.noreply.github.com>
2022-06-30 11:55:13 +0300

    Implemented dropdown menus for display by author or genre

--- bookStore/store/views.py
+++ bookStore/store/views.py
@@ -28,6 +28,12 @@ class CatalogView(ListView):
     def get_queryset(self):
         return models.Book.objects.all()
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        context['authors'] = models.Author.objects.all()
+        context['genres'] = models.Genre.objects.all()
+        return context
+
 
 class CreateReviewView(LoginRequiredMixin, CreateView):
     template_name = 'store/create_review.html'
@@ -42,18 +48,12 @@ class CreateReviewView(LoginRequiredMixin, CreateView):
 
 class BookByAuthorView(CatalogView):
     def get_queryset(self):
-        return models.Book.objects \
-            .filter(authors__slug=self.kwargs['slug']) \
-            .prefetch_related('genres') \
-            .prefetch_related('authors')
+        return models.Book.objects.filter(authors__slug=self.kwargs['slug'])
 
 
 class BookByGenreView(CatalogView):
     def get_queryset(self):
-        return models.Book.objects \
-            .filter(genres__slug=self.kwargs['slug']) \
-            .prefetch_related('genres') \
-            .prefetch_related('authors')
+        return models.Book.objects.filter(genres__slug=self.kwargs['slug'])
 
 
 class BookDetail(DetailView):
